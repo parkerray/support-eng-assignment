3f989dec172bd3fa24cf47a8a85de5bc265aedc9
Parker Thompson <parker.thompson@webflow.com>
2024-01-30 21:23:35 -0600

    moved to the source file

--- public/index.js
+++ public/index.js
@@ -1,39 +1,53 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 document
-	.querySelector('#main-form')
-	.addEventListener('submit', async function (event) {
-		// When the form is submitted, run this code
-		// Prevent the default form submission
-		event.preventDefault();
-
-		// Check to see if the style "newStyle" exists
-		let style = await webflow.getStyleByName('newStyle');
-		if (!style) {
-			// If the style doesn't exist, create it
-			style = webflow.createStyle('newStyle');
-
-			// Set the properties of the style
-			style.setProperties({
-				color: 'blue',
-				'font-size': '20px',
-				'font-family': 'Inconsolata',
-			});
-
-			// Save the style
-			await style.save();
-		}
-
-		// Get the selected element
-		const el = await webflow.getSelectedElement();
-
-		// Try to add the custom style
-		try {
-			// Add the style to the element and show a success notification
-			el.setStyles([style]);
-			await el.save();
-			webflow.notify({ type: 'Success', message: 'Style added!' });
-		} catch (e) {
-			// If there's an error, log it and show an error notification
-			console.error(e);
-			webflow.notify({ type: 'Error', message: 'Error adding style' });
-		}
-	});
+    .querySelector('#main-form')
+    .addEventListener('submit', function (event) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // When the form is submitted, run this code
+        // Prevent the default form submission
+        event.preventDefault();
+        // Check to see if the style "newStyle" exists
+        let style = yield webflow.getStyleByName('newStyle');
+        if (!style) {
+            // If the style doesn't exist, create it
+            style = webflow.createStyle('newStyle');
+            // Set the properties of the style
+            style.setProperties({
+                color: 'blue',
+                'font-size': '20px',
+                'font-family': 'Inconsolata',
+            });
+            // Save the style
+            yield style.save();
+        }
+        // Get the selected element
+        const el = yield webflow.getSelectedElement();
+        // Try to add the custom style
+        try {
+            // Check to see if an element is selected
+            if (el) {
+                // If an element is selected, add the style to the element and show a success notification
+                el.setStyles([style]);
+                yield el.save();
+                webflow.notify({ type: 'Success', message: 'Style added!' });
+            }
+            else {
+                // If no element is selected, show an info notification
+                webflow.notify({ type: 'Info', message: 'No element selected' });
+            }
+        }
+        catch (e) {
+            // If there's an error, log it and show an error notification
+            console.error(e);
+            webflow.notify({ type: 'Error', message: 'Error adding style' });
+        }
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,45 @@
-document.getElementById("lorem").onsubmit = async (event) => {
-  event.preventDefault()
-  // Get the currently selected element in the Designer
-  const el = await webflow.getSelectedElement()
-  if (el && el.textContent) {
-    // If we found the element and it has the ability to update the text content,
-    // replace it with some placeholder text
-    el.setTextContent(
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do " +
-        "eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    )
-    // Finally, save the changes to the element & they will be reflected in the Designer
-    await el.save()
-  }
-}
+document
+	.querySelector('#main-form')
+	.addEventListener('submit', async function (event) {
+		// When the form is submitted, run this code
+		// Prevent the default form submission
+		event.preventDefault();
+
+		// Check to see if the style "newStyle" exists
+		let style = await webflow.getStyleByName('newStyle');
+		if (!style) {
+			// If the style doesn't exist, create it
+			style = webflow.createStyle('newStyle');
+
+			// Set the properties of the style
+			style.setProperties({
+				color: 'blue',
+				'font-size': '20px',
+				'font-family': 'Inconsolata',
+			});
+
+			// Save the style
+			await style.save();
+		}
+
+		// Get the selected element
+		const el = await webflow.getSelectedElement();
+
+		// Try to add the custom style
+		try {
+      // Check to see if an element is selected
+			if (el) {
+        // If an element is selected, add the style to the element and show a success notification
+        el.setStyles([style]);
+			  await el.save();
+			  webflow.notify({ type: 'Success', message: 'Style added!' });
+      } else {
+        // If no element is selected, show an info notification
+        webflow.notify({ type: 'Info', message: 'No element selected' });
+      }
+		} catch (e) {
+			// If there's an error, log it and show an error notification
+			console.error(e);
+			webflow.notify({ type: 'Error', message: 'Error adding style' });
+		}
+	});
